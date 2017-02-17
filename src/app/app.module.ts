58d9dd4f46e937bf0b95df85509c87e07adc0abb
Trotyl <trotyl@qq.com>
2017-02-18 00:00:19 +0800

    Add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDc2PkvcTA4WL4kGyRVT96OooxFpCcnuDo',
+  authDomain: 'fir-blog-8c3fd.firebaseapp.com',
+  databaseURL: 'https://fir-blog-8c3fd.firebaseio.com',
+  storageBucket: 'fir-blog-8c3fd.appspot.com',
+  messagingSenderId: '678204448995'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
